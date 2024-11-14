7a223ffa7af914f226a2c925205818cbff4c85d3
Athira-S-Haridas <athiraharidas1199@gmail.com>
2024-11-14 10:56:22 -0800

    Add files via upload

---
+++ Java_script/scroll.js
@@ -0,0 +1,17 @@
+
+
+let first =document.getElementById('first')
+let second =document.getElementById('second')
+
+
+function forward(){
+    second.style.display='flex'
+    first.style.display='none'    
+}
+function backward(){
+    first.style.display='flex'
+    second.style.display='none'
+}
+
+
+
